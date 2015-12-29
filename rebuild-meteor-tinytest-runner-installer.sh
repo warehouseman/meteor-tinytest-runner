348fd97af0cc44399a18ae6f94fd8dc1128b68a3
Dude Awap <dude.awap@gmail.com>
2015-12-28 23:29:02 -0500

    try again to simplify webdriver installer

--- rebuild-meteor-tinytest-runner-installer.sh
+++ rebuild-meteor-tinytest-runner-installer.sh
@@ -34,9 +34,10 @@ meteorProjIfNotMeteorProj
 
 cp -R projMeteor tryMTRInst
 mkdir -p meteor-tinytest-runner_bk
-cp rebuild-meteor-tinytest-runner-installer.sh ./meteor-tinytest-runner
+# cp rebuild-meteor-tinytest-runner-installer.sh ./meteor-tinytest-runner
 cp -R ./meteor-tinytest-runner/* ./meteor-tinytest-runner_bk/
-cd ./meteor-tinytest-runner_bk/
+
+pushd ./meteor-tinytest-runner_bk/;
 rm -f LICENSE
 rm -f README.md
 rm -f rebuild-meteor-tinytest-runner-installer.sh
@@ -45,8 +46,9 @@ makeself --current . meteor-tinytest-runner.run "Installing meteor-tinytest-runn
 chmod a+x meteor-tinytest-runner.run
 cp meteor-tinytest-runner.run ../tryMTRInst/
 cp meteor-tinytest-runner.run ../meteor-tinytest-runner
-cd ..
-cd ./tryMTRInst
+popd;
+
+pushd ./tryMTRInst;
 ./meteor-tinytest-runner.run
 tree
 printf "Ready to test uninstall ? Hit <enter>."
@@ -55,7 +57,8 @@ read _
 ls -l
 printf "Done ? Hit <enter>."
 read _
-cd ..
+popd;
+
 rm -fr tryMTRInst
 rm -fr meteor-tinytest-runner_bk
 
